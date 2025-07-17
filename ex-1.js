3187f77c56f7e4e2cbbcbd480bafa72a073ab68c
kanyaweeN <40629441+kanyaweeN@users.noreply.github.com>
2025-07-17 14:13:01 +0700

    feat:add ex1-7

--- ex-1.js
+++ ex-1.js
@@ -1,7 +1,11 @@
 let colors = ["red", "green", "blue", "khaki", "orange"];
 
 // Start coding here
-let blueColor;
-let fourthColor;
-let fifthColor;
-let tenthColor;
+let blueColor = colors[2];
+let fourthColor = colors[3];
+let fifthColor = colors[4];
+let tenthColor = colors[9]; // This will be undefined since there is no 10th element
+console.log(blueColor); // Output: blue
+console.log(fourthColor); // Output: khaki
+console.log(fifthColor); // Output: orange
+console.log(tenthColor); // Output: undefined
